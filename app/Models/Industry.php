6870cfd74092c81aeaa5e94277f0f8f6718727c3
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-10-31 11:11:17 +0200

    Init Industries [IP]

---
+++ app/Models/Industry.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Industry extends Model {
+    use HasFactory, SoftDeletes, LogsActivity;
+
+    protected $fillable = ['title', 'description'];
+
+    public function getActivitylogOptions(): LogOptions {
+        return LogOptions::defaults();
+    }
+}
--- routes/v1.0/api.php
+++ routes/v1.0/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\IndustryController;
 use App\Http\Controllers\Api\NewsletterController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,16 @@ Route::group(['prefix' => 'auth'], function () {
         Route::get('attempts', [AuthController::class, "attempts"]);
     });
 });
+
+
+// Authenticated routes
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('industries', IndustryController::class);
+});
+
+// Public routes
+Route::apiResource('industries', IndustryController::class)->only(['index', 'show']);
+
 
 /* Landing Page APIs */
 // Newsletter
