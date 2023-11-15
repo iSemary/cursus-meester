24ccf261d5fbc247d38acab0e56d74db79f913c4
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-11-15 13:41:18 +0200

    Styling generated certificate WIP

--- modules/Courses/Entities/Certificate.php
+++ modules/Courses/Entities/Certificate.php
@@ -3,25 +3,97 @@
 namespace modules\Courses\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Certificate extends Model {
-    use HasFactory, SoftDeletes;
+    /** @var string */
+    public string $baseFilePath;
 
-    protected $fillable = [
-        'course_id',
-        'title',
-        'description',
-        'file_name',
-        'config',
-    ];
+    /** @var string */
+    public string $storeFilePath;
 
-    protected $casts = [
-        'config' => 'json',
-    ];
+    /** @var object */
+    private object $configuration;
 
-    public function course() {
-        return $this->belongsTo(Course::class, 'course_id');
+    /** @var string */
+    public string $certifyText;
+
+    public function __construct() {
+        $this->baseFilePath = public_path() . '/storage/certificates/base/certificate.pdf';
+        $this->storeFilePath = public_path() . '/storage/certificates/';
+
+        $this->setConfiguration();
+        $this->setCertifyText();
+    }
+
+    public function setConfiguration() {
+        $configuration = (object)[
+            'studentName' => (object)[
+                'position' => (object)['x' => 70, 'y' => 90],
+                'font' => (object)['size' => 30, 'family' => '', 'color' => [164, 130, 213]]
+            ],
+            'instructorName' => (object)[
+                'position' => (object)['x' => 100, 'y' => 120],
+                'font' => (object)['size' => 15, 'family' => '', 'color' => [0, 0, 0]]
+            ],
+            'certifyText' => (object)[
+                'position' => (object)['x' => 50, 'y' => 110],
+                'font' => (object)['size' => 20, 'family' => '', 'color' => [0, 0, 0]]
+            ],
+            'finishedDate' => (object)[
+                'position' => (object)['x' => '', 'y' => ''],
+                'font' => (object)['size' => '', 'family' => '', 'color' => '']
+            ],
+            'signature' => (object)[
+                'position' => (object)['x' => '', 'y' => ''],
+                'font' => (object)['size' => '', 'family' => '', 'color' => '']
+            ]
+        ];
+        // set the configuration
+        $this->configuration = $configuration;
+    }
+
+    // Get configuration details -> to generate new certificates
+    public function getConfiguration(): object {
+        return $this->configuration;
+    }
+
+    // Set Certify Text Template
+    public function setCertifyText(): void {
+        $this->certifyText = "This is to certify that CERT_STUDENT_NAME successfully \ncompleted CERT_TOTAL_HOURS of CERT_COURSE_NAME online course on CERT_FINISHED_DATE.";
+    }
+
+    // Get Certify Text
+    public function getCertifyText(string $studentName, string $courseName, int $courseDuration, string $finishedDate): string {
+        $certifyText = $this->certifyText;
+        // replace student name
+        $certifyText = str_replace('CERT_STUDENT_NAME', $studentName, $certifyText);
+        // replace course name
+        $certifyText = str_replace('CERT_COURSE_NAME', $this->formatTextByWords($courseName), $certifyText);
+        // replace course duration
+        $certifyText = str_replace('CERT_TOTAL_HOURS', ($courseDuration . ($courseDuration >= 60 ? ' hours' : ' minutes')), $certifyText);
+        // replace finished date
+        $certifyText = str_replace('CERT_FINISHED_DATE', $finishedDate, $certifyText);
+        return $certifyText;
+    }
+
+
+    /**
+     * The function takes string like course name, and implode \n after each 6 words to create a new line in the certificate style
+     * 
+     * @param string text The `text` parameter is a string that represents the text that needs to be
+     * formatted.
+     * 
+     * @return string a formatted string where the words from the input text are divided into lines of 6
+     * words each, separated by newlines.
+     */
+    private function formatTextByWords(string $text): string {
+        $words = explode(' ', $text);
+        $lines = [];
+        $chunks = array_chunk($words, 6);
+        foreach ($chunks as $chunk) {
+            $lines[] = implode(' ', $chunk);
+        }
+        $formattedText = implode("\n", $lines);
+        return $formattedText;
     }
 }
--- modules/Courses/Http/Controllers/Api/CertificateController.php
+++ modules/Courses/Http/Controllers/Api/CertificateController.php
@@ -7,28 +7,39 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use modules\Courses\Entities\Certificate;
+use modules\Courses\Entities\Course;
 use setasign\Fpdi\Tcpdf\Fpdi;
 
 class CertificateController extends ApiController {
+    private $certificate;
+
+    public function __construct(Certificate $certificate) {
+        $this->certificate = $certificate;
+    }
 
     public function claimCertificate(int $courseId): JsonResponse {
         // Check if user finished all videos
         $certificateUrl = '';
-
-
-        $data = [
-            'course_name' => 'Master backend development using Laravel 10',
-            'instructor_name' => 'Abdelrahman Mostafa',
-            'student_name' => 'Kevin Khaled Ahmed Mohammed',
-            'reference_number' => '478-WEZ-58A-CSL',
-            'total_hours' => '57',
-            'finished_at' => '04/11/2023',
-        ];
+        $user = User::first();
+        $course = Course::first();
+        $data = $this->prepareDataForCertificate($course, $user);
         $fileName = $this->generateNewCertificateFile($data);
         return $this->return(200, 'Certificate claimed successfully', ['certificate_url' => $certificateUrl]);
     }
 
     public function getCertificate(string $referenceNumber): JsonResponse {
+    }
+
+    private function prepareDataForCertificate(Course $course, User $user) {
+        return [
+            'course_name' => 'Master backend development using Laravel 10 Master backend development using Laravel 10',
+            'instructor_name' => 'Abdelrahman Mostafa',
+            'student_name' => 'Kevin Khaled Ahmed Mohammed',
+            'reference_number' => '478-WEZ-58A-CSL',
+            'course_duration' => '57',
+            'finished_at' => "04/11/2023",
+        ];
     }
 
     private function sendCertificateViaMail(string $referenceNumber, User $user): void {
@@ -40,23 +51,52 @@ class CertificateController extends ApiController {
     private function pushCertificateClaimedNotification(int $courseId, int $studentId): void {
     }
 
-    private function generateNewCertificateFile(array $data): string {
-        // clone original certificate and create a new file 
-        $fileName =  Str::uuid() . '.pdf';
 
+    private function generateNewCertificateFile(array $data): string {
+        $certificateConfiguration = $this->certificate->getConfiguration();
+
+        // generate random file name
+        $fileName =  Str::uuid() . '.pdf';
         // customize the data
         $pdf = new Fpdi();
-        $pdf->setSourceFile('path/to/existing.pdf');
-        $tpl = $pdf->importPage(1);
-        $pdf->AddPage();
-        $pdf->useTemplate($tpl);
+        // clone original certificate and create a new file 
+        $pdf->setSourceFile($this->certificate->baseFilePath);
+        // Get dimensions of the first page
+        $template = $pdf->importPage(1);
+        $size = $pdf->getTemplateSize($template);
+        // Create a new page with the same dimensions
+        $pdf->AddPage($size['orientation'], [$size['width'], $size['height']]);
+        // Import the first page from the original PDF
+        $pdf->useTemplate($template);
 
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Text(10, 10, 'Modified content');
+        /**
+         *   Add text to the imported page
+         */
+        /* Student Name*/
+        $pdf->SetFont($certificateConfiguration->studentName->font->family, '', $certificateConfiguration->studentName->font->size);
+        $pdf->SetTextColor($certificateConfiguration->studentName->font->color[0], $certificateConfiguration->studentName->font->color[1], $certificateConfiguration->studentName->font->color[2]);
+        $pdf->SetXY($certificateConfiguration->studentName->position->x, $certificateConfiguration->studentName->position->y);
+        $pdf->Cell(0, 10, $data['student_name'], 0, 1);
+        /* Certify Text */
+        $certifyText = $this->certificate->getCertifyText($data['student_name'], $data['course_name'], $data['course_duration'], $data['finished_at']);
+        $pdf->SetFont($certificateConfiguration->certifyText->font->family, '', $certificateConfiguration->certifyText->font->size);
+        $pdf->SetTextColor($certificateConfiguration->certifyText->font->color[0], $certificateConfiguration->certifyText->font->color[1], $certificateConfiguration->certifyText->font->color[2]);
+        $pdf->SetXY($certificateConfiguration->certifyText->position->x, $certificateConfiguration->certifyText->position->y);
+        $certifyTextLines = explode("\n", $certifyText);
+        $lineHeight = 10;
+        foreach ($certifyTextLines as $key => $line) {
+            $pdf->SetXY($certificateConfiguration->certifyText->position->x, $certificateConfiguration->certifyText->position->y + ($lineHeight * $key));
+            $pdf->Cell(0, $lineHeight, $line, 0, 1);
+        }
 
-        $pdf->Output('path/to/new_modified.pdf', 'F');
+        // /* Instructor Name*/
+        // $pdf->SetFont($certificateConfiguration->instructorName->font->family, '', $certificateConfiguration->instructorName->font->size);
+        // $pdf->SetTextColor($certificateConfiguration->instructorName->font->color[0], $certificateConfiguration->instructorName->font->color[1], $certificateConfiguration->instructorName->font->color[2]);
+        // $pdf->SetXY($certificateConfiguration->instructorName->position->x, $certificateConfiguration->instructorName->position->y);
+        // $pdf->Cell(0, 10, $data['instructor_name'], 0, 1);
 
-
+        // Save modified certificate file
+        $pdf->Output($this->certificate->storeFilePath . $fileName, 'F');
         return $fileName;
     }
 }
