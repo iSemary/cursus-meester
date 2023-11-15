1525849c2b2590bbe2eb92d8545784a849b14b26
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-11-15 17:43:51 +0200

    Added notification WIP

---
+++ app/Models/Notification.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Notification extends Model {
+    use HasFactory, SoftDeletes, Notifiable;
+
+    protected $fillable = ['user_id', 'type_id', 'notifiable', 'subject', 'body', 'data', 'read_at'];
+}
---
+++ app/Observers/NotificationObserver.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Notification;
+
+class NotificationObserver {
+
+    public function created(Notification $notification) {
+    }
+}
