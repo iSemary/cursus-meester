40ad4fd4448f6fd1b811d62069a4114d0b7f2300
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-11-20 14:49:17 +0200

    Finished authenticate middleware

--- resources/js/src/middleware.js
+++ resources/js/src/middleware.js
@@ -1,33 +1,41 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request, response) {
-    if (request.cookies.get("AUTH_TOKEN")) {
-        await fetch(process.env.NEXT_PUBLIC_API_URL + "auth/check", {
-            method: "GET",
-            headers: {
-                Authorization: "Bearer " + request.cookies.get("AUTH_TOKEN"),
-            },
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    return NextResponse.redirect(
-                        new URL("/login", request.url)
-                    );
-                } else {
-                    return NextResponse.redirect(
-                        new URL("/login", request.url)
-                    );
+    const authToken = request.cookies.get("AUTH_TOKEN");
+    if (authToken) {
+        try {
+            const apiResponse = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/auth/check`,
+                {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${authToken.value}`,
+                    },
                 }
-                // const data = response.json();
-                // console.log(data, "NEW-DATA");
-            })
-            .catch((error) => {
-                return NextResponse.redirect(new URL("/login", request.url));
-            });
+            );
+            if (!apiResponse.ok) {
+                // return redirectToLogin(request);
+            }
+        } catch (error) {
+            console.log("Error occurred while checking authentication:", error);
+            return redirectToLogin(request);
+        }
     } else {
-        return NextResponse.redirect(new URL("/login", request.url));
+        return redirectToLogin(request);
     }
 }
+function redirectToLogin(request) {
+    const loginURL = new URL("/login", request.url);
+    return NextResponse.redirect(loginURL);
+}
+/** Authenticated routes */
 export const config = {
-    matcher: ["/cart"],
+    matcher: [
+        "/cart",
+        "/wishlist",
+        "/settings",
+        "/notifications",
+        "/my-courses",
+        "/dashboard/:path*", // all dashboard routes
+    ],
 };
