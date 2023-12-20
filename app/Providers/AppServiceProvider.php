6d6404e5e453009c674e31b129b23f0a6ad918d0
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-12-20 12:08:42 +0200

    Enable https for laravel assets url (production only)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
