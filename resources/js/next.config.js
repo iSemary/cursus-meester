2db0fb0172fa4145059bc4b86ee95d9e42c65a00
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2023-12-20 12:17:07 +0200

    change next config

--- resources/js/next.config.js
+++ resources/js/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     staticPageGenerationTimeout: 10000,
     images: {
         domains: ["127.0.0.1", "placehold.co", "abdelrahman.online"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**.maxposter.ru",
+            },
+        ],
     },
     distDir: "_next",
 };
